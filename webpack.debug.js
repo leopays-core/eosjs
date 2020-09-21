12f10af6ebb3dbb9217e7c017f71d375f31764cd
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-09-21 21:53:14 +0700

    Update webpack.debug.js

--- webpack.debug.js
+++ webpack.debug.js
@@ -1,34 +1,34 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        leopaysjs_api: './src/eosjs-api.ts',
-        leopaysjs_jsonrpc: './src/rpc-web.ts',
-        leopaysjs_jssig: './src/eosjs-jssig.ts',
-        leopaysjs_numeric: './src/eosjs-numeric.ts',
-    },
-    devtool: 'inline-source-map',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: {
-                    loader: 'ts-loader',
-                    options: {
-                        configFile: 'tsconfig.web.json'
-                    }
-                },
-                exclude: /node_modules/,
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: x => x.chunk.name.replace('_', '-') + '-debug.js',
-        library: '[name]',
-        path: path.resolve(__dirname, 'dist-web', 'debug'),
-    }
+  entry: {
+    leopaysjs_api: './src/eosjs-api.ts',
+    leopaysjs_jsonrpc: './src/rpc-web.ts',
+    leopaysjs_jssig: './src/eosjs-jssig.ts',
+    leopaysjs_numeric: './src/eosjs-numeric.ts',
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.web.json'
+          }
+        },
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: x => x.chunk.name.replace('_', '-') + '.js',
+    library: '[name]',
+    path: path.resolve(__dirname, 'dist-web'),
+  }
 };
