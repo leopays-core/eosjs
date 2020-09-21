f90e9082e18e8f5c44d23aeaf5cefde980718544
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-09-21 22:59:54 +0700

    scripts

--- scripts/copy-ripe-md.js
+++ scripts/copy-ripe-md.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var root = __dirname.replace('scripts', '');
 
-if(!fs.existsSync(path.join(root + 'dist')))
-    fs.mkdirSync(path.join(root + 'dist'));
+if (!fs.existsSync(path.join(root + 'dist')))
+  fs.mkdirSync(path.join(root + 'dist'));
 
 fs.copyFileSync(path.join(root + 'src/ripemd.es5.js'), path.join(root + 'dist/ripemd.js'));
